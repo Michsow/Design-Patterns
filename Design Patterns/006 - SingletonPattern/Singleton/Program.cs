9402c2dce4375c84f67c9c930acc0e0ce358351e
Michusow <1194779@student.roc-nijmegen.nl>
2025-10-06 14:17:15 +0200

    singleton

---
+++ Design Patterns/006 - SingletonPattern/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler boiler = ChocolateBoiler.getInstance();
+            ChocolateBoiler boiler2 = ChocolateBoiler.getInstance();
+
+            boiler.fill();
+            boiler.boil();
+            boiler.drain();
+
+            Console.WriteLine(Object.ReferenceEquals(boiler, boiler2));
+        }
+    }
+}
